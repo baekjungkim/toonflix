95cf6e4a014c3cf26a05d5ed33e12fe4d2f4b6c6
Kim Baekjung <zvgandam@gmail.com>
2023-02-01 18:28:22 +0900

    #6.7 ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:toonflix/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -27,8 +27,22 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           return !snapshot.hasData
-              ? const Text("Loading...")
-              : const Text('Thre is data!');
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: snapshot.data!.length,
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(
+                      width: 20,
+                    );
+                  },
+                  itemBuilder: (context, index) {
+                    var webtoon = snapshot.data![index];
+                    return Text(webtoon.title);
+                  },
+                );
         },
       ),
     );
