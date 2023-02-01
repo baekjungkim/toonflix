e00e57c4c6541a20e6db75a93f9bcf3ffce06b4a
baekjungkim <zvgandam@gmail.com>
2023-02-02 00:58:33 +0900

    #6.15 Episodes

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -46,57 +46,123 @@ class _DetailScreenState extends State<DetailScreen> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CardWidget(
-                  thumb: widget.thumb,
-                  id: widget.id,
-                  size: 250,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            FutureBuilder(
-              future: webtoon,
-              builder: (context, snapshot) {
-                return !snapshot.hasData
-                    ? const Text('...')
-                    : Padding(
-                        padding: const EdgeInsets.fromLTRB(40, 20, 40, 100),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${snapshot.data!.genre} / ${snapshot.data!.age}',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            Text(
-                              snapshot.data!.about,
-                              style: const TextStyle(
-                                fontSize: 16,
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-              },
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 50,
+            horizontal: 40,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CardWidget(
+                    thumb: widget.thumb,
+                    id: widget.id,
+                    size: 250,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              FutureBuilder(
+                future: webtoon,
+                builder: (context, snapshot) {
+                  return !snapshot.hasData
+                      ? const Text('...')
+                      : makeInfo(snapshot);
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              FutureBuilder(
+                future: episodes,
+                builder: (context, snapshot) {
+                  return !snapshot.hasData
+                      ? Container()
+                      : Flexible(
+                          child: makeEpisodeList(snapshot),
+                        );
+                },
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Column makeInfo(AsyncSnapshot<WebtoonDetailModel> snapshot) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          snapshot.data!.about,
+          style: const TextStyle(
+            fontSize: 16,
+          ),
+        ),
+      ],
+    );
+  }
+
+  ListView makeEpisodeList(AsyncSnapshot<List<WebtoonEpisodeModel>> snapshot) {
+    return ListView.separated(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var episode = snapshot.data![index];
+        return Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.green.shade300,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(
+                color: Colors.green.shade300,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    episode.title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const Icon(
+                    Icons.chevron_right_rounded,
+                    color: Colors.white,
+                    weight: 600,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        height: 10,
       ),
     );
   }
